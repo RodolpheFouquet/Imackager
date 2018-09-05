255c8f7cae265f2331f9bebe48c71838f3300cee
Rodolphe Fouquet <rodolphe.fouquet@gpac-licensing.com>
2018-09-05 06:36:38 +0200

    First commit, transcoding + packaging started

---
+++ imackager.py
@@ -0,0 +1,38 @@
+#!/usr/bin/env python3
+
+import subprocess
+from flask import Flask, request, jsonify
+app = Flask(__name__)
+
+packagedDir= "packaged/"
+
+@app.route("/")
+def hello():
+    return "Imackager is running fine"
+
+@app.route("/package", methods=["POST"])
+def add_message():
+    content = request.json
+    resolutions = content["files"]["mainVideo"][0]["transcode"]
+    videoFile = content["files"]["mainVideo"][0]["url"]
+    for resolution in resolutions:
+        print("Transcoding the resolution " + str(resolution))
+        args = ["ffmpeg", "-y", "-i", videoFile, "-c:a", "aac",
+            "-vf", "scale=-2:"+str(resolution), "-c:v",
+			"libx264", "-bf", "0", "-crf", "22", packagedDir + str(resolution) +"_" + videoFile]
+        ret = subprocess.call(args)
+        if ret!= 0:
+            return "trancoding not ok"
+    mp4boxArgs = ["MP4Box", "-dash", "2000", "-profile", "live"]
+    videos = content["files"]["mainVideo"]
+    audios = content["files"]["audio"]
+    subtitles = content["files"]["subtitle"]
+
+    for video in videos:
+        mp4boxArgs = mp4boxArgs + [video["url"]]
+    for audio in audios:
+        mp4boxArgs = mp4boxArgs + [audio["url"]]
+    ret = subprocess.call(mp4boxArgs)
+    if ret != 0:
+        return "not ok"
+    return "ok"
